e765bad22e8249f4e380f344a09f32e42fbed4bd
Ahsan <johnyhawkahsan@gmail.com>
2019-12-12 10:36:50 +0500

    2nd Commit- Added Icons and SplashScreen. Now need to add basic functionality to app.

--- app/src/main/java/com/johnyhawkdesigns/a56_dentistapp/MainActivity.java
+++ app/src/main/java/com/johnyhawkdesigns/a56_dentistapp/MainActivity.java
@@ -3,12 +3,47 @@ package com.johnyhawkdesigns.a56_dentistapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static int SPLASH_TIMEOUT = 2000; //This is 2 seconds
+    Handler handler = new Handler();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // To add appropriate delay for splashScreen to stay there
+        try{
+            Thread.sleep(SPLASH_TIMEOUT);
+        } catch (Exception e){
+            Log.d(TAG, "onCreate: e = " + e);
+        }
+
+        // Make sure this is before calling super.onCreate
+        setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, SPLASH_TIMEOUT);
+
+        getSupportActionBar().setTitle("Dentist App");
+
+
+
+
+
     }
 }
