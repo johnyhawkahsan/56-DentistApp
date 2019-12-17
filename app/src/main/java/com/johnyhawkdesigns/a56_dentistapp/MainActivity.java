606f718282aef923ed6c092da2f83939cc9fff18
Ahsan <johnyhawkahsan@gmail.com>
2019-12-17 08:56:51 +0500

    4th commit- Added new Adaptive Icon for app. Also added basic Navigation drawerLayout which is currently not working due to Toolbar error which is because of SplashScreen theme's ActionBar conflicting with Toolbar of DrawerLayout.

--- app/src/main/java/com/johnyhawkdesigns/a56_dentistapp/MainActivity.java
+++ app/src/main/java/com/johnyhawkdesigns/a56_dentistapp/MainActivity.java
@@ -1,10 +1,22 @@
 package com.johnyhawkdesigns.a56_dentistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIMEOUT = 2000; //This is 2 seconds
     Handler handler = new Handler();
 
-
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +51,41 @@ public class MainActivity extends AppCompatActivity {
             }
         }, SPLASH_TIMEOUT);
 
-        getSupportActionBar().setTitle("Dentist App");
 
-
-
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home
+                //R.id.nav_gallery,
+                )
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+
+
 }
